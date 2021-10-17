2d475b4bb776d264fa6ec6dd8df1885a82089726
Makarand Patil <makrandpatil50@gmail.com>
2021-10-17 13:04:10 +0530

    The method can take two number and return sum

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -3,10 +3,27 @@ package calculator;
 class StringCalculator {
 
     public int add(String input) {
+    	
+    	String [] arr = input.split(",");
+    	
         if(input.isEmpty()) {
         	return 0;
-        }
-    	return Integer.parseInt(input);
+        }else if(arr.length == 1) {
+    		return Integer.parseInt(arr[0]);
+    	}
+    	else  {
+    		return getSum(arr);
+    	}
+    }
+    
+    private int getSum(String[] numbers)  {
+    	int sum = 0;
+    	for(int index = 0; index < numbers.length;index++) {
+    		sum = Integer.parseInt(numbers[index]) + sum;
+    		
+    	}
+    	
+    	return sum;
     }
 
 }
