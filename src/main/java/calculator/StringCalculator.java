347fa7c5e3b054c03e1e0838544ac2519f158d18
Makarand Patil <makrandpatil50@gmail.com>
2021-10-17 12:54:48 +0530

    The method can take one number

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -3,7 +3,10 @@ package calculator;
 class StringCalculator {
 
     public int add(String input) {
-        return 0;
+        if(input.isEmpty()) {
+        	return 0;
+        }
+    	return Integer.parseInt(input);
     }
 
 }
